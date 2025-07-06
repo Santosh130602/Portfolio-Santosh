727ce4ccb0d16ad3387e517c478189f464ad9777
santosh pal <santoshpal@santoshs-MacBook-Air.local>
2025-07-06 17:34:52 +0530

    update

---
+++ public/js/ddc0ccc304e78d7bf94f.js
@@ -0,0 +1,32 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkportfolio"] = self["webpackChunkportfolio"] || []).push([["src_app_views_PageNotFound_js"],{
+
+/***/ "./src/app/views/PageNotFound.js":
+/*!***************************************!*\
+  !*** ./src/app/views/PageNotFound.js ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _assets_images_hero2_png__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../assets/images/hero2.png */ \"./src/assets/images/hero2.png\");\n\n\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((data, locale) => {\n  return /*html*/ `\n    <div class=\"not-found-container\">\n      <img\n        src=\"${_assets_images_hero2_png__WEBPACK_IMPORTED_MODULE_0__}\"\n        alt=\"🤷‍♂️ 404 Not Found\"\n        class=\"not-found-image\"\n        style=\"width: 400px; height: auto;\"\n      />\n      <h1 class=\"not-found-title\">Oops! Page Not Found</h1>\n      <p class=\"not-found-text\">\n        ${locale.errors?.notFound || \"The page you're looking for doesn't exist.\"}\n      </p>\n    </div>\n  `;\n});\n\n\n//# sourceURL=webpack://portfolio/./src/app/views/PageNotFound.js?");
+
+/***/ }),
+
+/***/ "./src/assets/images/hero2.png":
+/*!*************************************!*\
+  !*** ./src/assets/images/hero2.png ***!
+  \*************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"images/4373f8b602c5a12e9a89.png\";\n\n//# sourceURL=webpack://portfolio/./src/assets/images/hero2.png?");
+
+/***/ })
+
+}]);
